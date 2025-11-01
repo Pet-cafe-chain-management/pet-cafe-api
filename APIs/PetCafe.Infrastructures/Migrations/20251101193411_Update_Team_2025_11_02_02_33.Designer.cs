0a3d2c32a6b6fe3b98618d5f187e59e1c64443b7
tannt-tv <tannt19092001@gmail.com>
2025-11-02 02:35:27 +0700

    update: work_shift table

--- APIs/PetCafe.Infrastructures/Migrations/20251101193411_Update_Team_2025_11_02_02_33.Designer.cs
+++ APIs/PetCafe.Infrastructures/Migrations/20251101193411_Update_Team_2025_11_02_02_33.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PetCafe.Infrastructures;
@@ -11,9 +12,11 @@ using PetCafe.Infrastructures;
 namespace PetCafe.Infrastructures.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251101193411_Update_Team_2025_11_02_02_33")]
+    partial class Update_Team_2025_11_02_02_33
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ APIs/PetCafe.Infrastructures/Migrations/20251101193411_Update_Team_2025_11_02_02_33.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetCafe.Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Team_2025_11_02_02_33 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "work_shifts");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "team_members");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "work_shifts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "team_members",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+    }
+}
